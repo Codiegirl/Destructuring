aaa179b90df85847cab5344ee8db2902fb54561f
github username <github email address>
2019-12-15 11:44:26 -0600

    solved

--- app.js
+++ app.js
@@ -14,3 +14,20 @@ const HIGH_TEMPERATURES = {
   console.log(today); // should be 77
   console.log(tomorrow); // should be 80
   
+
+//assign variables from objects
+  const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // change code below this line
+    
+  const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES; 
+  
+  // change code above this line
+  
+  console.log(yesterday) // should be not defined
+  console.log(highToday); // should be 77
+  console.log(highTomorrow); // should be 80
