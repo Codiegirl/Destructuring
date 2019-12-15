2649dcf03902cc371e0514ee92b968c4a422ad90
github username <github email address>
2019-12-15 11:58:18 -0600

    added rest parameter to reassign array elements

--- app.js
+++ app.js
@@ -58,3 +58,19 @@ const HIGH_TEMPERATURES = {
 // change code above this line
 console.log(a); // should be 6
 console.log(b); // should be 8
+
+
+
+//Rest Parameter to Reassign Array Elements
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  "use strict";
+  // change code below this line
+  const [a, b, ...arr] = list; // change this
+  // change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
+
